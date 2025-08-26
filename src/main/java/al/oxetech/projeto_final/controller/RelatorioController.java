f8b3da835d5a1675f27f55ac39b4334e6933d6f8
Ulpio <ulpionetto0@gmail.com>
2025-08-26 10:01:40 -0300

    Controllers

--- src/main/java/al/oxetech/projeto_final/controller/RelatorioController.java
+++ src/main/java/al/oxetech/projeto_final/controller/RelatorioController.java
@@ -2,16 +2,15 @@ package al.oxetech.projeto_final.controller;
 
 import al.oxetech.projeto_final.dto.relatorio.RelatorioDTO;
 import al.oxetech.projeto_final.dto.relatorio.RelatorioInputDTO;
-import al.oxetech.projeto_final.security.RelatorioService;
+import al.oxetech.projeto_final.service.RelatorioService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@RestController
+@RequestMapping("/relatorios")
 public class RelatorioController {
 
     private final RelatorioService relatorioService;
