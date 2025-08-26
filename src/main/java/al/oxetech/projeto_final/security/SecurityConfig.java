0de788d0c523e377ae61f308ffbb7dda9a343c8b
Ulpio <ulpionetto0@gmail.com>
2025-08-26 10:01:58 -0300

    Security (Liberar Rotas)

---
+++ src/main/java/al/oxetech/projeto_final/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package al.oxetech.projeto_final.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                ).csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
