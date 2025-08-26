5b6c8bec6f424b25b4aac7d8846ffab3472e8a1e
Ulpio <ulpionetto0@gmail.com>
2025-08-26 09:33:56 -0300

    Controllers

---
+++ src/main/java/al/oxetech/projeto_final/controller/RelatorioController.java
@@ -0,0 +1,33 @@
+package al.oxetech.projeto_final.controller;
+
+import al.oxetech.projeto_final.dto.relatorio.RelatorioDTO;
+import al.oxetech.projeto_final.dto.relatorio.RelatorioInputDTO;
+import al.oxetech.projeto_final.security.RelatorioService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+public class RelatorioController {
+
+    private final RelatorioService relatorioService;
+
+    public RelatorioController(RelatorioService relatorioService){
+        this.relatorioService = relatorioService;
+    }
+
+    @PostMapping
+    public ResponseEntity<RelatorioDTO> criar(@RequestBody @Valid RelatorioInputDTO dto){
+        RelatorioDTO novoRelatorio = relatorioService.salvar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoRelatorio);
+    }
+
+    @GetMapping
+    public List<RelatorioDTO> listarTodos(){
+        return relatorioService.listarTodos();
+    }
+}
---
+++ src/main/java/al/oxetech/projeto_final/controller/UsuarioController.java
@@ -0,0 +1,32 @@
+package al.oxetech.projeto_final.controller;
+
+import al.oxetech.projeto_final.dto.usuario.UsuarioDTO;
+import al.oxetech.projeto_final.dto.usuario.UsuarioInputDTO;
+import al.oxetech.projeto_final.security.UsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService){
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UsuarioDTO> criar(@RequestBody @Valid UsuarioInputDTO dto){
+        UsuarioDTO novoUsuario = usuarioService.salvar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoUsuario);
+    }
+
+    @GetMapping
+    public List<UsuarioDTO> listarTodos(){
+        return usuarioService.listarTodos();
+    }
+}
