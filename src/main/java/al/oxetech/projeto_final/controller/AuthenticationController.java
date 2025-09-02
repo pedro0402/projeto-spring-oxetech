0a36b070dbd2809cb9edccf5256f7086576c3d6a
Pedro Moraes <pedro693.ph@gmail.com>
2025-09-02 10:51:05 -0300

    feat: add AuthenticationController
    Adiciona o controller responsável por receber as requisições de login e
    iniciar o processo de autenticação do usuário no sistema.

---
+++ src/main/java/al/oxetech/projeto_final/controller/AuthenticationController.java
@@ -0,0 +1,20 @@
+package al.oxetech.projeto_final.controller;
+
+import al.oxetech.projeto_final.service.AuthenticationService;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/authenticate")
+    public String authenticate(Authentication authentication) {
+        return authenticationService.authenticate(authentication);
+    }
+}
