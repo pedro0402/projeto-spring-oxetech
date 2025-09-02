15b6ce2580a5e26334ca2eb42fb5e5b070b43281
Ulpio <ulpionetto0@gmail.com>
2025-09-02 18:21:38 -0300

    Adicionado /me

--- src/main/java/al/oxetech/projeto_final/controller/UsuarioController.java
+++ src/main/java/al/oxetech/projeto_final/controller/UsuarioController.java
@@ -2,7 +2,9 @@ package al.oxetech.projeto_final.controller;
 
 import al.oxetech.projeto_final.dto.usuario.UsuarioDTO;
 import al.oxetech.projeto_final.dto.usuario.UsuarioInputDTO;
+import al.oxetech.projeto_final.service.JwtService;
 import al.oxetech.projeto_final.service.UsuarioService;
+import io.jsonwebtoken.Claims;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,9 +19,11 @@ import java.util.List;
 @RequestMapping("/usuarios")
 public class UsuarioController {
     private final UsuarioService usuarioService;
+    private final JwtService jwtService;
 
-    public UsuarioController(UsuarioService usuarioService){
+    public UsuarioController(UsuarioService usuarioService,JwtService jwtService){
         this.usuarioService = usuarioService;
+        this.jwtService = jwtService;
     }
 
     /**
@@ -38,5 +42,15 @@ public class UsuarioController {
     @GetMapping
     public List<UsuarioDTO> listarTodos(){
         return usuarioService.listarTodos();
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UsuarioDTO> dadosDoUsuarioLogado(@RequestHeader("Authorization") String authHeader){
+        String token = authHeader.replace("Bearer ","");
+        Claims claims = jwtService.validarToken(token);
+        Long id = Long.parseLong(claims.getSubject());
+
+        UsuarioDTO usuarioDTO = usuarioService.buscarPorId(id);
+        return ResponseEntity.ok(usuarioDTO);
     }
 }
