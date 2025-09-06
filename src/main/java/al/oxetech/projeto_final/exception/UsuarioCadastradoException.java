3393ceb7db2aec485ff8f947907c73cd4d347d6e
Pedro Moraes <pedro693.ph@gmail.com>
2025-09-06 14:18:27 -0300

    feat(exception): cria classe UsuarioCadastradoException criação da classe responsável por lançar uma exceção personalizada quando já existe um usuário com mesmo nome e email

---
+++ src/main/java/al/oxetech/projeto_final/exception/UsuarioCadastradoException.java
@@ -0,0 +1,7 @@
+package al.oxetech.projeto_final.exception;
+
+public class UsuarioCadastradoException extends RuntimeException {
+    public UsuarioCadastradoException(String mensagem) {
+        super(mensagem);
+    }
+}
