3dcb0e704932f6db4c9f9ce86796af0dca177d21
Pedro Moraes <pedro693.ph@gmail.com>
2025-09-06 14:53:46 -0300

    chore(pom.xml): adiciona nova dependência do mapstruct
    adiciona dependência responsável por fazer o mapper das classes

--- src/main/java/al/oxetech/projeto_final/exception/UsuarioNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package al.oxetech.projeto_final.exception;
-
-public class UsuarioNotFoundException extends RuntimeException {
-    public UsuarioNotFoundException(String mensagem){
-        super(mensagem);
-    }
-}
