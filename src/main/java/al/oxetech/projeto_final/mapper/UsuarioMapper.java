d8dabe9a19b40ff5bdeac8fff945fc213f9d4a29
Pedro Moraes <pedro693.ph@gmail.com>
2025-09-06 20:46:02 -0300

    feat(mapper): adiciona classe UsuarioMapper
    criação da classe responsável pelo mapper de objetos

---
+++ src/main/java/al/oxetech/projeto_final/mapper/UsuarioMapper.java
@@ -0,0 +1,23 @@
+package al.oxetech.projeto_final.mapper;
+
+import al.oxetech.projeto_final.dto.usuario.UsuarioDTO;
+import al.oxetech.projeto_final.dto.usuario.UsuarioInputDTO;
+import al.oxetech.projeto_final.model.Usuario;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UsuarioMapper {
+
+    UsuarioDTO toUsuarioDTO(Usuario usuario);
+
+    UsuarioDTO toUsuarioDTO(UsuarioInputDTO usuarioInputDTO);
+
+    UsuarioInputDTO toUsuarioInputDTO(UsuarioDTO usuarioDTO);
+
+    UsuarioInputDTO toUsuarioInputDTO(Usuario usuario);
+
+    Usuario toEntity(UsuarioDTO usuarioDTO);
+
+    Usuario toEntity(UsuarioInputDTO usuarioInputDTO);
+
+}
