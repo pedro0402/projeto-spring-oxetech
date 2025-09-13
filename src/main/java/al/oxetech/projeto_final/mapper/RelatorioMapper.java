018e19b8057d12fb76d0b7a300f2c0b79053c76a
Pedro Moraes <pedro693.ph@gmail.com>
2025-09-13 01:54:57 -0300

    feat(mapper): adiciona interface para mappear relatórios
    criação de interface responsável por mappear objetos

---
+++ src/main/java/al/oxetech/projeto_final/mapper/RelatorioMapper.java
@@ -0,0 +1,14 @@
+package al.oxetech.projeto_final.mapper;
+
+import al.oxetech.projeto_final.dto.relatorio.RelatorioDTO;
+import al.oxetech.projeto_final.model.Relatorio;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface RelatorioMapper {
+
+    @Mapping(source = "autor.nome", target = "autorNome")
+    RelatorioDTO toRelatorioDTO(Relatorio relatorio);
+
+}
