c48b368e0be07391f1913c490460db0db61bf060
Pedro Moraes <pedro693.ph@gmail.com>
2025-09-16 00:44:14 -0300

    feat(mapper): adiciona método para mappear objetos
    criação do método toEntity para transformar de entidade para dto

--- src/main/java/al/oxetech/projeto_final/mapper/RelatorioMapper.java
+++ src/main/java/al/oxetech/projeto_final/mapper/RelatorioMapper.java
@@ -1,6 +1,7 @@
 package al.oxetech.projeto_final.mapper;
 
 import al.oxetech.projeto_final.dto.relatorio.RelatorioDTO;
+import al.oxetech.projeto_final.dto.relatorio.RelatorioPatchUpdateDTO;
 import al.oxetech.projeto_final.model.Relatorio;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -10,5 +11,7 @@ public interface RelatorioMapper {
 
     @Mapping(source = "autor.nome", target = "autorNome")
     RelatorioDTO toRelatorioDTO(Relatorio relatorio);
+
+    Relatorio toEntity(RelatorioPatchUpdateDTO relatorioPatchUpdateDTO);
 
 }
